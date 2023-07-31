901dd94fea3ad835383ccec77840a4637cc09c31
Cem Savas <c.savas59@gmail.com>
2023-07-31 14:37:09 +0300

    food CRUD operations

--- vendor/urls.py
+++ vendor/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path, include
-from .views import vprofile, menu_builder, fooditems_by_category, add_category, edit_category, delete_category
+from .views import vprofile, menu_builder, fooditems_by_category, add_category, edit_category, delete_category, add_food, edit_food, delete_food
 from accounts import views as AccountViews
 
 urlpatterns = [
@@ -12,4 +12,9 @@ urlpatterns = [
     path('menu-builder/category/add/', add_category, name='add_category'),
     path('menu-builder/category/edit/<int:pk>', edit_category, name='edit_category'),
     path('menu-builder/category/delete/<int:pk>', delete_category, name='delete_category'),
+
+    # food item crud
+    path('menu-builder/food/add/', add_food, name='add_food'),
+    path('menu-builder/food/edit/<int:pk>', edit_food, name='edit_food'),
+    path('menu-builder/food/delete/<int:pk>', delete_food, name='delete_food'),
 ]
